72a9bdc49bbaa065d6f61743674acb264577da4e
David Paulo <davidpfsmartins@gmail.com>
2024-12-28 14:17:40 -0300

    OrderItem, many-to-many association with extra attributes

--- workshop-springboot3-jpa-main/src/main/java/com/projeto22/curso/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto22.curso.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto22.curso.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- workshop-springboot3-jpa-main/src/main/java/com/projeto22/curso/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto22.curso.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto22.curso.entities.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
